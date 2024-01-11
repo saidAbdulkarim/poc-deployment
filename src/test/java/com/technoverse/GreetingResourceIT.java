d35393539da18f9a992518bdd688c6216a60ab5a
said <ssaid@techno-verse.com>
2024-01-11 14:43:21 +0200

    jj

---
+++ src/test/java/com/technoverse/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+//package com.technoverse;
+//
+//import io.quarkus.test.junit.QuarkusIntegrationTest;
+//
+//@QuarkusIntegrationTest
+//class GreetingResourceIT extends GreetingResourceTest {
+//    // Execute the same tests but in packaged mode.
+//}
